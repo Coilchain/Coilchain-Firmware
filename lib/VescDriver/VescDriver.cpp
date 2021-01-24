7475496ca9752307a3ba4669e9afbf15c3cf59b6
Matthias Grob <maetugr@gmail.com>
2021-01-24 17:32:05 +0100

    Implement VESC driver
    Tested requesting version, telemetry
    and commanding current and break current.

---
+++ lib/VescDriver/VescDriver.cpp
@@ -0,0 +1,250 @@
+/**
+ * @copyright Copyright (c) 2021 Coilchain Development Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * @file VescDriverUart.cpp
+ * @author Matthias Grob <maetugr@gmail.com>
+ */
+
+#include "VescDriver.hpp"
+#include <memory.h>
+
+void VescDriver::commandCurrent(float current)
+{
+	uint8_t command[5]{VescCommand::SET_CURRENT};
+	uint16_t index{1};
+	insertInt32(command, index, static_cast<int32_t>(current * 1000.f));
+	sendPacket(command, 5);
+}
+
+void VescDriver::commandBrakeCurrent(float current)
+{
+	uint8_t command[5]{VescCommand::SET_CURRENT_BRAKE};
+	uint16_t index{1};
+	insertInt32(command, index, static_cast<int32_t>(current * 1000.f));
+	sendPacket(command, 5);
+}
+
+void VescDriver::requestFirmwareVersion()
+{
+	uint8_t command{VescCommand::FW_VERSION};
+	sendPacket(&command, 1);
+}
+
+void VescDriver::requestValues()
+{
+	uint8_t command{VescCommand::GET_VALUES};
+	sendPacket(&command, 1);
+}
+
+void VescDriver::processInput()
+{
+	size_t length_to_read{1};
+	uint8_t buffer_input[length_to_read];
+	size_t length_read = read(buffer_input, length_to_read);
+	//printf("length read: %d\n", length_read);
+	for (size_t i = 0; i < length_read; i++) {
+		parseInputByte(buffer_input[i]);
+	}
+}
+
+size_t VescDriver::sendPacket(const uint8_t *payload, const uint16_t payload_length)
+{
+	if (payload_length > MAXIMUM_PAYLOAD_LENGTH) {
+		return 0;
+	}
+
+	uint8_t packet_buffer[payload_length + PACKET_OVERHEAD_LENGTH];
+	uint16_t index{0};
+
+	// Start byte and payload size
+	if (payload_length <= 256) {
+		packet_buffer[index++] = 2;
+		packet_buffer[index++] = payload_length;
+	} else {
+		packet_buffer[index++] = 3;
+		packet_buffer[index++] = static_cast<uint8_t>(payload_length >> 8);
+		packet_buffer[index++] = static_cast<uint8_t>(payload_length & 0xFF);
+	}
+
+	// Payload
+	memcpy(&packet_buffer[index], payload, payload_length);
+	index += payload_length;
+
+	// CRC
+	const uint16_t crc = crc16(payload, payload_length);
+	packet_buffer[index++] = static_cast<uint8_t>(crc >> 8);
+	packet_buffer[index++] = static_cast<uint8_t>(crc & 0xFF);
+
+	// Stop byte
+	packet_buffer[index++] = 3;
+
+	return write(packet_buffer, index);
+}
+
+void VescDriver::parsePayload(const uint8_t *payload, const uint16_t payload_length)
+{
+	//printf("ID: %d Size: %d\n", payload[0], payload_length);
+	uint16_t index{1};
+	switch (payload[0]) {
+	case VescCommand::FW_VERSION:
+		if (payload_length >= 9u) {
+			_vesc_version.version_major = payload[index++];
+			_vesc_version.version_minor = payload[index++];
+			// strcpy(_vesc_version.hardware_name, reinterpret_cast<const char *>(&payload[index]));
+			// index += strlen(_vesc_version.hardware_name) + 1u;
+			// memcpy(_vesc_version.stm32_uuid_8, &payload[index], sizeof(_vesc_version.stm32_uuid_8));
+			// index += 12;
+			// _vesc_version.pairing_done = payload[index++];
+			// _vesc_version.test_version_number = payload[index++];
+			// _vesc_version.hardware_type = payload[index++];
+			// _vesc_version.custom_configuration = payload[index++];
+		}
+		break;
+	case VescCommand::GET_VALUES:
+		if (payload_length >= 73u) {
+			_vesc_values.fet_temperature = extractFloat16(payload, index) / 10.f;
+			_vesc_values.motor_temperature = extractFloat16(payload, index) / 10.f;
+			_vesc_values.motor_current = extractFloat32(payload, index) / 100.f;
+			_vesc_values.input_current = extractFloat32(payload, index) / 100.f;
+			_vesc_values.reset_average_id = extractFloat32(payload, index) / 100.f;
+			_vesc_values.reset_average_iq = extractFloat32(payload, index) / 100.f;
+			_vesc_values.duty_cycle = extractFloat16(payload, index) / 1000.f;
+			_vesc_values.rpm = extractInt32(payload, index);
+			_vesc_values.input_voltage = extractFloat16(payload, index) / 10.f;
+			_vesc_values.used_charge_Ah = extractFloat32(payload, index) / 1e4f;
+			_vesc_values.charged_charge_Ah = extractFloat32(payload, index) / 1e4f;
+			_vesc_values.used_energy_Wh = extractFloat32(payload, index) / 1e4f;
+			_vesc_values.charged_energy_wh = extractFloat32(payload, index) / 10.f;
+			_vesc_values.tachometer = extractInt32(payload, index);
+			_vesc_values.tachometer_absolute = extractInt32(payload, index);
+			_vesc_values.fault = payload[index++];
+			_vesc_values.position_pid = extractFloat32(payload, index) / 1e6f;
+			_vesc_values.controller_id = payload[index++];
+			_vesc_values.ntc_temperature_mos1 = extractFloat16(payload, index) / 10.f;
+			_vesc_values.ntc_temperature_mos2 = extractFloat16(payload, index) / 10.f;
+			_vesc_values.ntc_temperature_mos3 = extractFloat16(payload, index) / 10.f;
+			_vesc_values.read_reset_average_vd = extractFloat32(payload, index) / 1000.f;
+			_vesc_values.read_reset_average_vq = extractFloat32(payload, index) / 1000.f;
+		}
+		break;
+	}
+}
+
+void VescDriver::parseInputByte(uint8_t byte)
+{
+	if (_input_byte_index == 0) {
+		// Start byte
+		if (byte == 2 || byte == 3) {
+			_input_start_byte = byte;
+			_input_byte_index++;
+		}
+
+	} else if (_input_byte_index == 1) {
+		// Payload size
+		_input_byte_index++;
+
+		if (_input_start_byte == 2) {
+			// Short packet
+			_input_payload_length = byte;
+		} else {
+			// Long packet high byte
+			_input_payload_length = byte << 8;
+		}
+
+	} else if (_input_byte_index == 2 && _input_start_byte == 3) {
+		// Payload size long packet low byte
+		_input_payload_length |= byte;
+		_input_byte_index++;
+
+		if (_input_payload_length > MAXIMUM_PAYLOAD_LENGTH) {
+			_input_byte_index = 0;
+		}
+
+	} else if (_input_byte_index < _input_start_byte + _input_payload_length) {
+		// Payload
+		_input_payload[_input_byte_index - _input_start_byte] = byte;
+		_input_byte_index++;
+
+	} else if (_input_byte_index == _input_start_byte + _input_payload_length) {
+		// CRC high byte
+		_input_payload_crc = byte << 8;
+		_input_byte_index++;
+
+	} else if (_input_byte_index == _input_start_byte + _input_payload_length + 1u) {
+		// CRC low byte
+		_input_payload_crc |= byte;
+		_input_byte_index++;
+
+		if (_input_payload_crc != crc16(_input_payload, _input_payload_length)) {
+			_input_byte_index = 0;
+		}
+
+	} else if (_input_byte_index == _input_start_byte + _input_payload_length + 2u) {
+		// Stop byte
+		_input_byte_index = 0;
+
+		if (byte == 3) {
+			parsePayload(_input_payload, _input_payload_length);
+		}
+	}
+}
+
+uint16_t VescDriver::crc16(const uint8_t *buffer, const uint16_t length)
+{
+	uint16_t checksum{0};
+
+	for (size_t i = 0; i < length; i++) {
+		uint8_t table_index = (((checksum >> 8) ^ buffer[i]) & 0xFF);
+		checksum = CRC_TABLE[table_index] ^ (checksum << 8);
+	}
+
+	return checksum;
+}
+
+void VescDriver::insertInt32(uint8_t *buffer, uint16_t &index, int32_t value)
+{
+	buffer[index++] = value >> 24;
+	buffer[index++] = value >> 16;
+	buffer[index++] = value >> 8;
+	buffer[index++] = value;
+}
+
+int16_t VescDriver::extractInt16(const uint8_t *buffer, uint16_t &index)
+{
+	index += 2;
+	return static_cast<int16_t>(buffer[index - 2] << 8 | buffer[index - 1]);
+}
+
+float VescDriver::extractFloat16(const uint8_t *buffer, uint16_t &index)
+{
+	return static_cast<float>(extractInt16(buffer, index));
+}
+
+int32_t VescDriver::extractInt32(const uint8_t *buffer, uint16_t &index)
+{
+	index += 4;
+	return static_cast<int32_t>(buffer[index - 4] << 24 | buffer[index - 3] << 16 | buffer[index - 2] << 8 | buffer[index - 1]);
+}
+
+float VescDriver::extractFloat32(const uint8_t *buffer, uint16_t &index)
+{
+	return static_cast<float>(extractInt32(buffer, index));
+}
+
+size_t VescDriver::write(const uint8_t *buffer, const uint16_t length) {
+	return fwrite(buffer, sizeof(uint8_t), length, _device);
+}
+
+
+size_t VescDriver::read(uint8_t *buffer, const uint16_t length) {
+	return fread(buffer, sizeof(uint8_t), length, _device);
+}
---
+++ lib/VescDriver/VescDriver.hpp
@@ -0,0 +1,70 @@
+/**
+ * @copyright Copyright (c) 2021 Coilchain Development Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * @file VescDriverUart.hpp
+ * @brief Driver to communicate over UART with VESC motor contollers
+ * @details More about VESC BLDC (brushless direct current) electric motor controllers by Benjamin Vedder on https://vesc-project.com/
+ * @author Matthias Grob <maetugr@gmail.com>
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include <stdio.h>
+#include "VescProtocol.h"
+
+class VescDriver {
+public:
+	VescDriver(FILE *device) : _device(device) {};
+	~VescDriver() { fclose(_device); };
+
+	void commandCurrent(float current);
+	void commandBrakeCurrent(float current);
+
+	void requestFirmwareVersion();
+	void requestValues();
+	float getRpm() { return _vesc_values.rpm; };
+	float getInputVoltage() { return _vesc_values.input_voltage; };
+
+	void processInput(); ///< call when data is ready to read
+
+private:
+	// de-/serialize packets
+	size_t sendPacket(const uint8_t *payload, const uint16_t payload_length);
+	void parsePayload(const uint8_t *payload, const uint16_t payload_length);
+	void parseInputByte(uint8_t byte);
+	uint16_t crc16(const uint8_t *buffer, const uint16_t length);
+
+	// big-endian helpers
+	void insertInt32(uint8_t *buffer, uint16_t &index, int32_t value);
+	int16_t extractInt16(const uint8_t *buffer, uint16_t &index);
+	float extractFloat16(const uint8_t *buffer, uint16_t &index);
+	int32_t extractInt32(const uint8_t *buffer, uint16_t &index);
+	float extractFloat32(const uint8_t *buffer, uint16_t &index);
+
+	// byte stream access through _device
+	size_t write(const uint8_t *buffer, const uint16_t length);
+	size_t read(uint8_t *buffer, const uint16_t length);
+
+	FILE *_device;
+
+	// input packet parsing
+	size_t _input_byte_index{0}; ///< keeps track of the input packets parsing state
+	uint8_t _input_start_byte{0};
+	uint16_t _input_payload_length{0};
+	uint8_t _input_payload[MAXIMUM_PAYLOAD_LENGTH];
+	uint16_t _input_payload_crc{0};
+
+	// information storage for getters
+	VescVersion _vesc_version{};
+	VescValues _vesc_values{};
+};
